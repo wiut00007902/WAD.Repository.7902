368da5ef50c03fe3e6ee5437174a27b5b5dbc7aa
00007902 <saidakhmedov@yahoo.com>
2021-03-31 18:32:25 +0100

    v. 1.0
    - Added database:
    1) Created Database Access Layer
    2) Created DbContext class
    3) Created service in Startup class
    4) Added database connection string in appsettings.json file
    
    - Created controllers
    1) Created controller for Department model
    2) Created controller for Employee model
    3) Created controller for Regions model
    4) Created controller for Tasks model
    5) Moved data autoset logics to controllers
    
    - Created views
    1) Created CRUD views for all models

---
+++ ManagementApplication/Controllers/RegionsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ManagementApplication.DAL;
+using ManagementApplication.Models;
+
+namespace ManagementApplication.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly ManagementApplicationDbContext _context;
+
+        public RegionsController(ManagementApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Regions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Regions.ToListAsync());
+        }
+
+        // GET: Regions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var region = await _context.Regions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return View(region);
+        }
+
+        // GET: Regions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Regions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,RegionName,CreationDate")] Region region)
+        {
+            if (ModelState.IsValid)
+            {
+                region.CreationDate = DateTime.Now;
+                _context.Add(region);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(region);
+        }
+
+        // GET: Regions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var region = await _context.Regions.FindAsync(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            return View(region);
+        }
+
+        // POST: Regions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,RegionName,CreationDate")] Region region)
+        {
+            if (id != region.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(region);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RegionExists(region.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(region);
+        }
+
+        // GET: Regions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var region = await _context.Regions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return View(region);
+        }
+
+        // POST: Regions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var region = await _context.Regions.FindAsync(id);
+            _context.Regions.Remove(region);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RegionExists(int id)
+        {
+            return _context.Regions.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ManagementApplication/DAL/ManagementApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ManagementApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementApplication.DAL
+{
+    public class ManagementApplicationDbContext : DbContext
+    {
+        public ManagementApplicationDbContext(DbContextOptions<ManagementApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Region> Regions { get; set; }
+        public virtual DbSet<Models.Task> Tasks { get; set; }
+    }
+}
