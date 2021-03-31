8ea0abdabc7488209c5835168bf48dc8b75a6bce
00007902 <saidakhmedov@yahoo.com>
2021-03-31 17:34:10 +0100

    Revision of Employee and Region models
    Changes in the Employee class:
    - Added data annotations and error messages
    
    Changes in the Region class:
    - Added data annotations and error messages
    - Commented the code

--- ManagementApplication/Models/Employee.cs
+++ ManagementApplication/Models/Employee.cs
@@ -8,19 +8,33 @@ namespace ManagementApplication.Models
 {
     public class Employee
     {
+        // ID of the employee
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please, insert the first name"), MinLength(2, ErrorMessage = "First name must be at least 2 symbols")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please, insert the last name"), MinLength(2, ErrorMessage = "Last name must be at least 2 symbols")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Please, insert the date of birth")]
         public DateTime DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Please, insert the gender")]
         public Gender Gender { get; set; }
+        [Required(ErrorMessage = "Please, insert the passport number")]
         public string PassportNo { get; set; }
+        [Required(ErrorMessage = "Please, insert the address")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "Please, insert the phone number"), Phone]
         public string Phone { get; set; }
+        [Required(ErrorMessage = "Please, insert the email"), EmailAddress]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Please, insert the employee's department")]
         public int DepartmentId { get; set; }
+        [Required(ErrorMessage = "Please, insert the employee's position")]
         public string Position { get; set; }
+        [Required(ErrorMessage = "Please, insert the employee's salary")]
         public float Salary { get; set; }
+        [Required(ErrorMessage = "Please, select the schedule of the work")]
         public Schedule Schedule { get; set; }
+        [Required(ErrorMessage = "Please, insert the manager ID")]
         public int ManagerId { get; set; }
         public DateTime EmployedOnDate { get; set; }
         public bool IsManager { get; set; }
--- ManagementApplication/Models/Region.cs
+++ ManagementApplication/Models/Region.cs
@@ -8,8 +8,13 @@ namespace ManagementApplication.Models
 {
     public class Region
     {
+        // ID of the region
         public int Id { get; set; }
+        // Data annotation that requires user to input region name with at least 2 symbols
+        [Required(ErrorMessage = "Please, insert the name of the region"), MinLength(2, ErrorMessage = "Name of the region should be at least 2 symbols")]
+        // Name of the region
         public string RegionName { get; set; }
+        // Region creation date. Will be set automatically
         public DateTime CreationDate { get; set; }
     }
 }
